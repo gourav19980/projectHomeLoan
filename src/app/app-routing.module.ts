9e2abe76a38a3e387d2777051a651c7bdc078f48
madhurighatage <ghatagemadhuri@gmail.com>
2022-02-20 11:14:28 +0530

    commited by Madhuri

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './Components/about-us/about-us.component';
+import { CalculatorComponent } from './Components/calculator/calculator.component';
+import { ContactUsComponent } from './Components/contact-us/contact-us.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { LoanComponent } from './Components/loan/loan.component';
 
-const routes: Routes = [];
+import { HomeLoanComponent } from './Components/home-loan/home-loan.component';
+
+const routes: Routes = [
+  {path: 'home', component: HomePageComponent},
+  {path: 'contactUs', component: ContactUsComponent},
+  {path: 'aboutUs', component: AboutUsComponent},
+  {path: 'loan', component: LoanComponent},
+  {path: 'calculator', component: CalculatorComponent},
+  {path: 'homeloan', component:HomeLoanComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './Components/home-page/home-page.component';
+import { LoanComponent } from './Components/loan/loan.component';
+import { AboutUsComponent } from './Components/about-us/about-us.component';
+import { ContactUsComponent } from './Components/contact-us/contact-us.component';
+import { CalculatorComponent } from './Components/calculator/calculator.component';
+import { HomeLoanComponent } from './Components/home-loan/home-loan.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    LoanComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+    CalculatorComponent,
+    HomeLoanComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
